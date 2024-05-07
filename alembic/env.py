b5ae75740562700da5879e781de740bb1be280ee
FrolovAlex22 <frolov.bsk@yandex.ru>
2024-05-07 14:28:31 +0500

    completed the 5th block. creating and applying migration

--- alembic/env.py
+++ alembic/env.py
@@ -2,11 +2,11 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from app.models.models import Base
 
 from alembic import context
 
 from app.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from app.models.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- app/models/models.py
+++ app/models/models.py
@@ -1,24 +1,30 @@
 from datetime import datetime
 from sqlalchemy import JSON, MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column
+from sqlalchemy.orm import declarative_base
 
-metadata = MetaData()
+Base = declarative_base()
 
-roles = Table(
-    'roles',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String, nullable=False),
-    Column('permissions', JSON)
-)
+class Roles(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
 
 
-users = Table(
-    'users',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('email', String, nullable=False),
-    Column('username', String, nullable=False),
-    Column('password', String, nullable=False),
-    Column('registered_ad', TIMESTAMP, default=datetime.now),
-    Column('role_id', Integer, ForeignKey('roles.id'))
-)
+class User(Base):
+    __tablename__ = 'users'
+    id = Column('id', Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    registered_ad = Column(TIMESTAMP, default=datetime.utcnow),
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    count = Column(Integer, nullable=False)
